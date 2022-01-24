b718f2395ef1399c92053f573ecaf438ad647a90
Jacobo Mateo Bedoya Oquendo <jacobobedoya@gmail.com>
2022-01-24 18:54:35 -0500

    adding some stuff

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-from simple_tools import print_debug
+from simple_tools import (
+    print_debug,
+    logging_debug,
+)
 
 @print_debug(prefix="**")
 def adition(a: int,b: int):
     return a+b
 
+@logging_debug(prefix="*")
+def multiplication(a: int, b: int):
+    return a*b
+
 def main():
-    a = "s"
-    b = "b"
+    a = 1
+    b = 2
     print(adition(a,b))
+    print(multiplication(a,b))
 
 if __name__ == '__main__':
     main()
